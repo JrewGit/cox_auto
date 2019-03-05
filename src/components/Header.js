57f7a123ce8e64cdbb641c979a22589319df9347
Jrew <jrewmohamed@gmail.com>
2019-03-05 16:37:38 -0500

    Added Header

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  color: {
+    backgroundColor: "#282829"
+  },
+  links: {
+    textDecoration: "none",
+    color: "white"
+  }
+};
+
+function Header(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.color}>
+        <Toolbar>
+          <Typography variant="h4" color="inherit" className={classes.grow}>
+            Cox Automotive
+          </Typography>
+          <Button color="inherit"><a className={classes.links} href="#Form">Sign Up</a></Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
